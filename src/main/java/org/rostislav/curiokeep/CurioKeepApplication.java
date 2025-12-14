726f0e8100671037a144e6d09643201362bcb36a
Rostislav Raykov <rostislav.raykov@infinno.eu>
2025-12-15 01:04:39 +0200

    Initial commit

---
+++ src/main/java/org/rostislav/curiokeep/CurioKeepApplication.java
@@ -0,0 +1,13 @@
+package org.rostislav.curiokeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurioKeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurioKeepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rostislav/curiokeep/CurioKeepApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rostislav.curiokeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurioKeepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
